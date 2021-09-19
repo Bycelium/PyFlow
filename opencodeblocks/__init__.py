2ea84f5ebc2d2a91feba9e629ce85e9b1f48abb0
Mathïs Fédérico <mathfederico@gmail.com>
2021-09-19 14:18:07 +0200

    :memo: Add OpenCodeBlocks package docstring

--- opencodeblocks/__init__.py
+++ opencodeblocks/__init__.py
@@ -2,6 +2,8 @@
 # OpenCodeBlock an open-source tool for modular visual programing in python
 # Copyright (C) 2021 Mathïs FEDERICO <https://www.gnu.org/licenses/>
 
-__name__ = 'OpenCodeBlocks'
+""" OpenCodeBlocks: An open-source tool for modular visual programing in python """
+
+__appname__ = 'OpenCodeBlocks'
 __author__ = 'Mathïs Fédérico'
 __version__ = '0.0.1'
