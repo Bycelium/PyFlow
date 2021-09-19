64dc87d3767d3b73c9e306a67e765a3213efed62
Mathïs Fédérico <mathfederico@gmail.com>
2021-09-19 14:08:12 +0200

    :umbrella: Add instanciation package test Add tests structure

---
+++ tests/integration/test_package.py
@@ -0,0 +1,8 @@
+# OpenCodeBlock an open-source tool for modular visual programing in python
+# Copyright (C) 2021 Mathïs FEDERICO <https://www.gnu.org/licenses/>
+
+""" Tests for the opencodeblocks package initialization. """
+
+def test_package_initialization():
+    """ The opencodeblocks package should instantiate correctly. """
+    import opencodeblocks as ocb
---
+++ tests/unit/__init__.py
@@ -0,0 +1,4 @@
+# OpenCodeBlock an open-source tool for modular visual programing in python
+# Copyright (C) 2021 Mathïs FEDERICO <https://www.gnu.org/licenses/>
+
+""" Unit tests for the opencodeblocks package. """
