62503ecfdb6d9b321d399301225f76d57012f382
Mathïs Fédérico <mathfederico@gmail.com>
2021-09-19 14:11:14 +0200

    :tada: Add opencodeblocks package structure

---
+++ opencodeblocks/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# OpenCodeBlock an open-source tool for modular visual programing in python
+# Copyright (C) 2021 Mathïs FEDERICO <https://www.gnu.org/licenses/>
+
+__name__ = 'OpenCodeBlocks'
+__author__ = 'Mathïs Fédérico'
+__version__ = '0.0.1'
