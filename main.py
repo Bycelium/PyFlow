81d36ee0f3199f591283245cd0e264e0c5f9e36a
Mathïs Fédérico <mathfederico@gmail.com>
2021-09-27 12:30:10 +0200

    :wrench: Refactor main.py

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 # OpenCodeBlock an open-source tool for modular visual programing in python
 # Copyright (C) 2021 Mathïs FEDERICO <https://www.gnu.org/licenses/>
 
-from opencodeblocks.core.node import Node
-from opencodeblocks.graphics.block import OCBBlock
 import os, sys
+
 from qtpy.QtWidgets import QApplication
 
-sys.path.insert(0, os.path.join( os.path.dirname(__file__), "..", ".." ))
-
+from opencodeblocks.core.node import CodeNode
+from opencodeblocks.graphics.block import OCBBlock
 from opencodeblocks.graphics.window import OCBWindow
+
+sys.path.insert(0, os.path.join( os.path.dirname(__file__), "..", ".." ))
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
@@ -16,7 +17,7 @@ if __name__ == '__main__':
 
     wnd = OCBWindow()
 
-    test_block = OCBBlock(Node("Test Block with a very very very long long name"))
+    test_block = OCBBlock(CodeNode(title="Test Block with a very very very long long name"))
     wnd.scene.addItem(test_block)
 
     wnd.show()
