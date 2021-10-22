e2982f162596e3e23a62f5fbf3449f743fe645c3
Mathïs Fédérico <mathfederico@gmail.com>
2021-10-22 13:50:25 +0200

    :tada: Improve main example

--- main.py
+++ main.py
@@ -14,7 +14,13 @@ from opencodeblocks.graphics.window import OCBWindow
 sys.path.insert(0, os.path.join( os.path.dirname(__file__), "..", ".." ))
 
 SOURCE_TEST = \
-'''def absolute_chicken(a, b):
+'''def build_dataset(a, b):
+    """ Build you dataset. """
+    return [0, a, 0, b, 1, a]
+'''
+
+SOURCE_TEST_2 = \
+'''def mon_ia(a, b):
     """ Compute the absolute value of inputs difference. """
     if a > b:
         return a - b
@@ -29,12 +35,11 @@ if __name__ == '__main__':
     wnd = OCBWindow()
     scene = wnd.ocb_widget.scene
 
-    test_block = OCBBlock(title="Test Block with a very very very long long name")
-    for _ in range(3):
-        test_block.add_socket(OCBSocket(test_block, socket_type='input'))
-    wnd.ocb_widget.scene.addItem(test_block)
+    test_block = OCBBlock(title="Other kind of block")
+    scene.addItem(test_block)
+    test_block.setPos(-250, 150)
 
-    test_block_2 = OCBCodeBlock(title="Test Block 2", source=SOURCE_TEST)
+    test_block_2 = OCBCodeBlock(title="Dataset", source=SOURCE_TEST)
     for _ in range(2):
         test_block_2.add_socket(OCBSocket(test_block_2, socket_type='input'))
     for _ in range(1):
@@ -42,12 +47,20 @@ if __name__ == '__main__':
     test_block_2.setPos(-350, -100)
     scene.addItem(test_block_2)
 
-    for i in range(3):
-        edge = OCBEdge(
-            source_socket=test_block_2.sockets_out[0],
-            destination_socket=test_block.sockets_in[i]
-        )
-        scene.addItem(edge)
+    test_block_3 = OCBCodeBlock(title="Mon IA (par blocks ?)", source=SOURCE_TEST_2)
+    for _ in range(2):
+        test_block_3.add_socket(OCBSocket(test_block_3, socket_type='input'))
+    for _ in range(1):
+        test_block_3.add_socket(OCBSocket(test_block_3, socket_type='output'))
+    test_block_3.setPos(0, -100)
+    scene.addItem(test_block_3)
+
+    # for i in range(3):
+    #     edge = OCBEdge(
+    #         source_socket=test_block_3.sockets_out[0],
+    #         destination_socket=test_block_2.sockets_in[i]
+    #     )
+    #     scene.addItem(edge)
 
     scene.save('scene.ipyg')
     scene.load('scene.ipyg')
