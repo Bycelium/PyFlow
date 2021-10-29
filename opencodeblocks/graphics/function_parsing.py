9e4d582eccafae5b5ccb1984587e53a9bb20e319
Alexandre Sajus <asajus47@gmail.com>
2021-10-29 15:09:32 +0200

    :hammer: Changes requested last commit

--- opencodeblocks/graphics/function_parsing.py
+++ opencodeblocks/graphics/function_parsing.py
@@ -3,7 +3,8 @@ from IPython.testing.globalipapp import get_ipython
 from IPython.utils.io import capture_output
 ip = get_ipython()
 
-def run_cell(cell:str):
+
+def run_cell(cell: str):
     """
     Executes a string of code in an IPython shell and returns the execution result
 
@@ -19,7 +20,8 @@ def run_cell(cell:str):
     res_out = io.stdout
     return res_out
 
-def get_function_name(code:str) -> str:
+
+def get_function_name(code: str) -> str:
     """
     Parses a string of code and returns the first function name it finds
 
@@ -29,11 +31,18 @@ def get_function_name(code:str) -> str:
     Return:
         Name of first defined function
     """
-    start_of_name = code.index("def") + 4
-    end_of_name = code.index("(")
+    def_index = code.find("def")
+    if def_index == -1:
+        raise ValueError("'def' not found in source code")
+    start_of_name = def_index + 4
+    parenthesis_index = code.find("(", start_of_name)
+    if parenthesis_index == -1:
+        raise ValueError("'(' not found in source code")
+    end_of_name = parenthesis_index
     return code[start_of_name:end_of_name]
 
-def get_signature(code:str) -> str:
+
+def get_signature(code: str) -> str:
     """
     Returns the signature of a string of Python code defining a function
     For example: the signature of def hello(a,b,c=3) is "(a,b,c=3)"
@@ -50,11 +59,34 @@ def get_signature(code:str) -> str:
     run_cell("from inspect import signature")
     return run_cell(f"print(signature({name}))")
 
-def extract_args(code:str) -> tuple:
+
+def find_kwarg_index(signature_couple):
+    """
+    Returns the index delimiting the args and kwargs in a list of arguments
+    Examples:
+        find_kwwarg_index(['a','b','c=3']) -> 2
+        find_kwwarg_index([]) -> None
+
+    Args:
+        list of Strings representing the arguments of a function
+
+    Return:
+        index delimiting the args and kwargs in a list of arguments
+
+    """
+    kwarg_index = len(signature_couple)
+    for i, item in enumerate(signature_couple):
+        if "=" in item:
+            kwarg_index = i
+            break
+    return kwarg_index
+
+
+def extract_args(code: str) -> tuple:
     """
     Returns the args and kwargs of a string of Python code defining a function
     Examples:
-        get_signature(def hello(a,b,c=3)) -> "(a,b,c=3)"
+        get_signature(def hello(a,b,c=3)...) -> "(a,b,c=3)"
 
     Args:
         code: String containing Python code
@@ -64,20 +96,19 @@ def extract_args(code:str) -> tuple:
 
     """
     signature_string = get_signature(code)
+    print(signature_string)
     # Remove parentheses
     signature_string = signature_string[1:-2]
-    signature_string = signature_string.replace(" ","")
+    signature_string = signature_string.replace(" ", "")
     if signature_string == "":
         return ([], [])
-    signature_couple = signature_string.split(",")
-    kwarg_index = len(signature_couple)
-    for i, item in enumerate(signature_couple):
-        if "=" in item:
-            kwarg_index = i
-            break
-    return signature_couple[:kwarg_index], signature_couple[kwarg_index:]
+    signature_list = signature_string.split(",")
+    kwarg_index = find_kwarg_index(signature_list)
+    print(kwarg_index)
+    return signature_list[:kwarg_index], signature_list[kwarg_index:]
 
-def execute_function(code:str, *args, **kwargs) -> str:
+
+def execute_function(code: str, *args, **kwargs) -> str:
     """
     Executes the function defined in code in an IPython shell and runs it fed by args and kwargs.
     Other arguments than the first are passed to the function when executing it.
@@ -99,3 +130,6 @@ def execute_function(code:str, *args, **kwargs) -> str:
 
     run_cell(code)
     return run_cell(execution_code + ')')
+
+
+print(execute_function("def function(a,b,c=10):\n return a+b+c", 10, 5))
--- tests/unit/scene/test_function_parsing.py
+++ tests/unit/scene/test_function_parsing.py
@@ -4,19 +4,85 @@ import pytest
 from pytest_mock import MockerFixture
 import pytest_check as check
 
-from opencodeblocks.graphics.function_parsing import run_cell, get_function_name, get_signature, extract_args, execute_function
+from opencodeblocks.graphics.function_parsing import (find_kwarg_index, run_cell,
+                                                      get_function_name,
+                                                      get_signature,
+                                                      extract_args,
+                                                      execute_function,
+                                                      find_kwarg_index)
+
 
 class TestFunctionParsing:
-    def test_run_cell(self, mocker:MockerFixture):
-        check.equal(run_cell("print(10)"), '10\n')
-    
-    def test_get_function_name(self, mocker:MockerFixture):
-        check.equal(get_function_name("def function():\n   return 'Hello'"), 'function')
-        check.equal(get_function_name("#Hello\ndef function():\n   return 'Hello'\na = 10"), 'function')
-        check.equal(get_function_name("#Hello\ndef function(a,b=10):\n   return 'Hello'\na = 10"), 'function')
 
-    def test_extract_args(self, mocker:MockerFixture):
-        mocker.patch('opencodeblocks.graphics.function_parsing.get_signature', return_value="()\n")
-        check.equal(extract_args("def function(a,b,c = 10):\n   return 'Hello'"), ([],[]))
-        mocker.patch('opencodeblocks.graphics.function_parsing.get_signature', return_value="(a,b,c = 10)\n")
-        check.equal(extract_args("def function(a,b,c = 10):\n   return 'Hello'"), (["a","b"],["c=10"]))
+    """Testing function_parsing functions"""
+
+    def test_run_cell(self, mocker: MockerFixture):
+        """ Test run_cell """
+        check.equal(run_cell("print(10)"), '10\n')
+
+    def test_get_function_name(self, mocker: MockerFixture):
+        """ Test get_function_name """
+        check.equal(get_function_name(
+            "def function():\n   return 'Hello'"), 'function')
+        check.equal(get_function_name(
+            "#Hello\ndef function():\n   return 'Hello'\na = 10"), 'function')
+        check.equal(get_function_name(
+            "#Hello\ndef function(a,b=10):\n   return 'Hello'\na = 10"), 'function')
+
+    def test_get_function_name_error(self, mocker: MockerFixture):
+        """ Return ValueError if get_function_name has wrong input """
+        with pytest.raises(ValueError):
+            get_function_name("")
+            get_function_name("#Hello")
+            get_function_name("def function")
+
+    def test_get_signature(self, mocker: MockerFixture):
+        """ Test get_signature """
+        mocker.patch(
+            'opencodeblocks.graphics.function_parsing.run_cell', return_value="(a, b, c=10)\n")
+        check.equal(get_signature(
+            "def function(a,b, c=10):\n return None"), "(a, b, c=10)\n")
+
+    def test_find_kwarg_index(self, mocker: MockerFixture):
+        """ Test find_kwarg_index """
+        check.equal(find_kwarg_index(['a', 'b', 'c=10']), 2)
+        check.equal(find_kwarg_index([]), 0)
+
+    def test_extract_args(self, mocker: MockerFixture):
+        """ Test extract_args """
+        mocker.patch(
+            'opencodeblocks.graphics.function_parsing.get_signature', return_value="()\n")
+        mocker.patch(
+            'opencodeblocks.graphics.function_parsing.find_kwarg_index', return_value=0)
+        check.equal(extract_args(
+            "def function():\n   return 'Hello'"), ([], []))
+        mocker.patch('opencodeblocks.graphics.function_parsing.get_signature',
+                     return_value="(a,b,c = 10)\n")
+        mocker.patch(
+            'opencodeblocks.graphics.function_parsing.find_kwarg_index', return_value=2)
+        check.equal(extract_args(
+            "def function(a,b,c = 10):\n   return 'Hello'"), (["a", "b"], ["c=10"]))
+
+    def test_extract_args_empty(self, mocker: MockerFixture):
+        """ Return a couple of empty lists if signature is empty """
+        mocker.patch('opencodeblocks.graphics.function_parsing.get_signature',
+                     return_value="()\n")
+        mocker.patch(
+            'opencodeblocks.graphics.function_parsing.find_kwarg_index', return_value=None)
+        check.equal(extract_args(
+            "def function( ):\n   return 'Hello'"), ([], []))
+        mocker.patch('opencodeblocks.graphics.function_parsing.get_signature',
+                     return_value="()\n")
+        mocker.patch(
+            'opencodeblocks.graphics.function_parsing.find_kwarg_index', return_value=None)
+        check.equal(extract_args(
+            "def function():\n   return 'Hello'"), ([], []))
+
+    def test_execute_function(self, mocker: MockerFixture):
+        """ Test execute_function """
+        mocker.patch('opencodeblocks.graphics.function_parsing.get_function_name',
+                     return_value="function")
+        mocker.patch(
+            'opencodeblocks.graphics.function_parsing.run_cell', return_value="Out[1]: 25\n")
+        check.equal(execute_function(
+            "def function(a,b,c=10):\n return a+b+c", 10, 5), "Out[1]: 25\n")
