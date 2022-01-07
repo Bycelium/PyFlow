c61c393ac7d20c28d9f05e21a2f7d58cc092ffc9
Mathïs Fédérico <mathfederico@gmail.com>
2022-01-07 18:56:55 +0100

    :tada: Add setup.py for setuptools package building

---
+++ setup.py
@@ -0,0 +1,41 @@
+# Pyflow an open-source tool for modular visual programing in python
+# Copyright (C) 2021-2022 Bycelium <https://www.gnu.org/licenses/>
+
+import pathlib
+from setuptools import setup, find_packages
+
+
+def get_version():
+    version_file = open("VERSION")
+    return version_file.read().strip()
+
+
+def get_requirements():
+    requirements_file = open("requirements.txt")
+    return requirements_file.readlines()
+
+
+HERE = pathlib.Path(__file__).parent  # The directory containing this file
+README = (HERE / "PYPI_README.md").read_text()
+VERSION = get_version()
+REQUIREMENTS = get_requirements()
+
+setup(
+    name="byc-pyflow",
+    version=VERSION,
+    author="Bycelium",
+    author_email="mathis.federico@bycelium.com",
+    description="An open-source tool for modular visual programing in python",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Bycelium/PyFlow",
+    packages=find_packages(exclude=["*test*", "*docs*"]),
+    include_package_data=True,
+    install_requires=REQUIREMENTS,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+)
