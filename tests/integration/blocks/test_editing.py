432c85b687db3018f03a5777c9642ce816e21fd0
Fabien Roger <efab.roger@gmail.com>
2022-01-09 23:57:32 +0100

    :umbrella: Fix tests and add tests for the history bug

--- tests/integration/blocks/test_editing.py
+++ tests/integration/blocks/test_editing.py
@@ -5,6 +5,7 @@
 Integration tests for the Blocks.
 """
 
+import time
 import pytest
 import pyautogui
 from pytestqt.qtbot import QtBot
@@ -23,71 +24,129 @@ class TestEditing:
     def setup(self):
         """Setup reused variables."""
         start_app(self)
-        self.code_block = CodeBlock(title="Testing block")
-        self.markdown_block = MarkdownBlock(title="Testing block")
+        self.code_block = CodeBlock(title="Testing code block 1")
+        self.code_block_2 = CodeBlock(title="Testing code block 2")
+        self.markdown_block = MarkdownBlock(title="Testing markdown block")
 
     def test_write_code_blocks(self, qtbot: QtBot):
         """code blocks can be written in."""
 
-        test_write_in(self, self.code_block, qtbot)
+        self.aux_test_write_in(self.code_block, qtbot)
 
     def test_write_markdown_blocks(self, qtbot: QtBot):
         """code blocks can be written in."""
 
-        test_write_in(self, self.markdown_block, qtbot)
+        self.aux_test_write_in(self.markdown_block, qtbot)
 
+    def test_history_not_lost(self, qtbot: QtBot):
+        """code blocks keep their own undo history."""
 
-def test_write_in(self: TestEditing, block: Block, qtbot: QtBot):
-    def test_write(self, qtbot: QtBot):
-        """can be written in."""
-        self._widget.scene.addItem(block)
-        self._widget.view.horizontalScrollBar().setValue(block.x())
-        self._widget.view.verticalScrollBar().setValue(
-            block.y() - self._widget.view.height() + block.height
-        )
+        self._widget.scene.addItem(self.code_block)
+        self._widget.scene.addItem(self.code_block_2)
+        self.code_block.setY(200)
+        self.code_block_2.setY(-200)
 
-        def testing_write(msgQueue: CheckingQueue):
+        def testing_history(msgQueue: CheckingQueue):
             # click inside the block and write in it
 
-            pos_block = QPointF(block.pos().x(), block.pos().y())
-
-            pos_block.setX(pos_block.x() + block.width / 2)
-            pos_block.setY(pos_block.y() + block.height / 2)
-
-            pos_block = self._widget.view.mapFromScene(pos_block)
-            pos_block = self._widget.view.mapToGlobal(pos_block)
-
-            pyautogui.moveTo(pos_block.x(), pos_block.y())
-            pyautogui.mouseDown(button="left")
-            pyautogui.mouseUp(button="left")
-            pyautogui.press(key="a")
-            pyautogui.press(key="b")
-            pyautogui.press(key="enter")
-            pyautogui.press(key="a")
-
-            msgQueue.check_equal(
-                block.source_editor.text(),
-                "ab\na",
-                "The chars have been written properly",
-            )
-            with pyautogui.hold("ctrl"):
-                pyautogui.press(key="z")
-
-            msgQueue.check_equal(
-                block.source_editor.text(),
-                "ab",
-                "undo worked properly",
+            pos_block_1 = QPointF(self.code_block.pos().x(), self.code_block.pos().y())
+            pos_block_2 = QPointF(
+                self.code_block_2.pos().x(), self.code_block_2.pos().y()
             )
 
+            pos_block_1.setX(self.code_block.x() + self.code_block.width / 2)
+            pos_block_1.setY(self.code_block.y() + self.code_block.height / 2)
+
+            pos_block_2.setX(self.code_block_2.x() + self.code_block_2.width / 2)
+            pos_block_2.setY(self.code_block_2.y() + self.code_block_2.height / 2)
+
+            pos_block_1 = self._widget.view.mapFromScene(pos_block_1)
+            pos_block_1 = self._widget.view.mapToGlobal(pos_block_1)
+
+            pos_block_2 = self._widget.view.mapFromScene(pos_block_2)
+            pos_block_2 = self._widget.view.mapToGlobal(pos_block_2)
+
+            pyautogui.moveTo(pos_block_1.x(), pos_block_1.y())
+            pyautogui.click()
+            pyautogui.press(["a", "b", "enter", "a"])
+
+            pyautogui.moveTo(pos_block_2.x(), pos_block_2.y())
+            pyautogui.click()
+            pyautogui.press(["c", "d", "enter", "d"])
+
+            pyautogui.moveTo(pos_block_1.x(), pos_block_1.y())
+            pyautogui.click()
             with pyautogui.hold("ctrl"):
-                pyautogui.press(key="z")
+                pyautogui.press("z")
 
             msgQueue.check_equal(
-                block.source_editor.text(),
-                "ab\na",
-                "redo worked properly",
+                self.code_block.source_editor.text().replace("\r", ""),
+                "ab\n",
+                "undo done properly",
+            )
+
+            pyautogui.moveTo(pos_block_2.x(), pos_block_2.y())
+            pyautogui.click()
+            with pyautogui.hold("ctrl"):
+                pyautogui.press("z")
+
+            msgQueue.check_equal(
+                self.code_block_2.source_editor.text().replace("\r", ""),
+                "cd\n",
+                "undo done properly",
             )
 
             msgQueue.stop()
 
-        apply_function_inapp(self.window, testing_write)
+        apply_function_inapp(self.window, testing_history)
+
+    def aux_test_write_in(self, block: Block, qtbot: QtBot):
+        def test_write(self, qtbot: QtBot):
+            """can be written in."""
+            self._widget.scene.addItem(block)
+            self._widget.view.horizontalScrollBar().setValue(block.x())
+            self._widget.view.verticalScrollBar().setValue(
+                block.y() - self._widget.view.height() + block.height
+            )
+
+            def testing_write(msgQueue: CheckingQueue):
+                # click inside the block and write in it
+
+                pos_block = QPointF(block.pos().x(), block.pos().y())
+
+                pos_block.setX(pos_block.x() + block.width / 2)
+                pos_block.setY(pos_block.y() + block.height / 2)
+
+                pos_block = self._widget.view.mapFromScene(pos_block)
+                pos_block = self._widget.view.mapToGlobal(pos_block)
+
+                pyautogui.moveTo(pos_block.x(), pos_block.y())
+                pyautogui.click()
+                pyautogui.press(["a", "b", "enter", "a"])
+
+                msgQueue.check_equal(
+                    block.source_editor.text().replace("\r", ""),
+                    "ab\na",
+                    "The chars have been written properly",
+                )
+                with pyautogui.hold("ctrl"):
+                    pyautogui.press("z")
+
+                msgQueue.check_equal(
+                    block.source_editor.text().replace("\r", ""),
+                    "ab",
+                    "undo worked properly",
+                )
+
+                with pyautogui.hold("ctrl"):
+                    pyautogui.press("y")
+
+                msgQueue.check_equal(
+                    block.source_editor.text().replace("\r", ""),
+                    "ab\na",
+                    "redo worked properly",
+                )
+
+                msgQueue.stop()
+
+            apply_function_inapp(self.window, testing_write)
