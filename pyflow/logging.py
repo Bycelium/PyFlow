f3337f08cdd39a880fbb380d7eed8a96f0ce23dc
Mathïs Fédérico <mathfederico@gmail.com>
2022-01-28 00:18:33 +0100

    :sparkles: Fix pylint issues :memo: Add logging docstrings

--- pyflow/logging.py
+++ pyflow/logging.py
@@ -1,19 +1,23 @@
-from ast import For
+# Pyflow an open-source tool for modular visual programing in python
+# Copyright (C) 2021-2022 Bycelium <https://www.gnu.org/licenses/>
+
+"""Utilitaries for logging in Pyflow."""
+
 from time import time
 import logging
 from functools import wraps
-from unittest.mock import DEFAULT
 from colorama import Fore, Style
 
 
 def log_init_time(logger: logging.Logger, level=logging.DEBUG):
+    """Decorator for logging an class init time."""
+
     def inner(func):
         @wraps(func)
         def wrapper_func(self: type, *args, **kwargs):
             init_time = time()
             func(self, *args, **kwargs)
-            # TODO: Make this more robust
-            class_name = str(self).split(" ")[0].split(".")[-1]
+            class_name = str(self).split(" ", maxsplit=1)[0].split(".")[-1]
             logger.log(
                 level, "Built %s in %.3fs", class_name, time() - init_time, stacklevel=2
             )
@@ -23,11 +27,21 @@ def log_init_time(logger: logging.Logger, level=logging.DEBUG):
     return inner
 
 
-def get_logger(name: str):
+def get_logger(name: str) -> logging.Logger:
+    """Get the logger for the current module given it's name.
+
+    Args:
+        name (str): Name of the module usualy obtained using '__main___'
+
+    Returns:
+        logging.Logger: Logger for the given module name.
+    """
     return logging.getLogger(name)
 
 
 class PyflowHandler(logging.StreamHandler):
+
+    """Custom logging handler for Pyflow."""
 
     COLOR_BY_LEVEL = {
         "DEBUG": Fore.GREEN,
@@ -39,7 +53,7 @@ class PyflowHandler(logging.StreamHandler):
         "CRITICAL": Fore.RED,
     }
 
-    def emit(self, record):
+    def emit(self, record: logging.LogRecord):
         record.pathname = "pyflow" + record.pathname.split("pyflow")[-1]
 
         level_color = self.COLOR_BY_LEVEL.get(record.levelname)
