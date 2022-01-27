3712453976040e750a860eca293fb8aa0b659e2f
Mathïs Fédérico <mathfederico@gmail.com>
2022-01-27 18:11:08 +0100

    :tada: Add logging & improve CLI

--- pyflow/__main__.py
+++ pyflow/__main__.py
@@ -6,6 +6,10 @@
 
 import os
 import sys
+
+import argparse
+import logging
+
 import asyncio
 
 if os.name == "nt":  # If on windows
@@ -19,11 +23,44 @@ from pyflow import __version__
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), "..", ".."))
 
 if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-p", "--path", type=str, help="path to a file to open")
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        type=str,
+        choices=logging._nameToLevel.keys(),
+        help="set logging level",
+        default="INFO",
+    )
+    args = parser.parse_args()
+
+    # Debug flag will lower logging level to DEBUG
+    log_level = logging._nameToLevel[args.verbose.upper()]
+    logging.basicConfig(
+        filename="pyflow.log",
+        level=logging.INFO,
+    )
+    pyflow_logger = logging.getLogger("pyflow")
+    pyflow_logger.setLevel(log_level)
+
+    stream_formater = logging.Formatter(
+        "%(levelname)s:%(name)s:%(asctime)s > %(message)s", datefmt="%I:%M:%S"
+    )
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(stream_formater)
+    pyflow_logger.addHandler(stream_handler)
+
+    if log_level <= logging.DEBUG:
+        print("-" * 15 + " DEBUG MODE ON " + "-" * 15)
+
     app = QApplication(sys.argv)
     app.setStyle("Fusion")
     wnd = Window()
-    if len(sys.argv) > 1:
-        wnd.createNewMdiChild(sys.argv[1])
+
+    if args.path:
+        wnd.createNewMdiChild(args.path)
 
     wnd.setWindowTitle(f"Pyflow {__version__}")
     wnd.show()
