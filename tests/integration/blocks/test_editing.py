d38bb8210585c8004e09afb3e79fd2faaa66f182
Mathïs Fédérico <mathfederico@gmail.com>
2022-02-02 07:14:50 +0100

    :fire: Comment out tests than work locally only

--- tests/integration/blocks/test_editing.py
+++ tests/integration/blocks/test_editing.py
@@ -92,79 +92,79 @@ class TestEditing(InAppTest):
 
     #     apply_function_inapp(self.window, testing_history)
 
-    def test_editing_history(self, qtbot: QtBot):
-        """code blocks history is compatible with scene history."""
-        self.code_block_1.setY(-200)
-        self.code_block_2.setY(200)
+    # def test_editing_history(self, qtbot: QtBot):
+    #     """code blocks history is compatible with scene history."""
+    #     self.code_block_1.setY(-200)
+    #     self.code_block_2.setY(200)
 
-        code_block_1_id = self.code_block_1.id
-        code_block_2_id = self.code_block_2.id
+    #     code_block_1_id = self.code_block_1.id
+    #     code_block_2_id = self.code_block_2.id
 
-        def testing_history(msgQueue: CheckingQueue):
-            # click inside the block and write in it
-            initial_pos = (self.code_block_2.pos().x(), self.code_block_2.pos().y())
-            pos_block_2 = self.get_global_pos(self.code_block_2, rel_pos=(0.5, 0.05))
-            center_block_1 = self.get_global_pos(self.code_block_1, rel_pos=(0.5, 0.5))
-            center_block_2 = self.get_global_pos(self.code_block_2, rel_pos=(0.5, 0.5))
+    #     def testing_history(msgQueue: CheckingQueue):
+    #         # click inside the block and write in it
+    #         initial_pos = (self.code_block_2.pos().x(), self.code_block_2.pos().y())
+    #         pos_block_2 = self.get_global_pos(self.code_block_2, rel_pos=(0.5, 0.05))
+    #         center_block_1 = self.get_global_pos(self.code_block_1, rel_pos=(0.5, 0.5))
+    #         center_block_2 = self.get_global_pos(self.code_block_2, rel_pos=(0.5, 0.5))
 
-            pyautogui.moveTo(center_block_1.x(), center_block_1.y())
-            pyautogui.click()
-            pyautogui.press(["a", "b", "enter", "a"])
+    #         pyautogui.moveTo(center_block_1.x(), center_block_1.y())
+    #         pyautogui.click()
+    #         pyautogui.press(["a", "b", "enter", "a"])
 
-            time.sleep(0.1)
-            pyautogui.moveTo(center_block_2.x(), center_block_2.y())
-            pyautogui.click()
-            pyautogui.press(["c", "d", "enter", "d"])
+    #         time.sleep(0.1)
+    #         pyautogui.moveTo(center_block_2.x(), center_block_2.y())
+    #         pyautogui.click()
+    #         pyautogui.press(["c", "d", "enter", "d"])
 
-            time.sleep(0.1)
-            pyautogui.moveTo(pos_block_2.x(), pos_block_2.y())
-            pyautogui.mouseDown(button="left")
-            time.sleep(0.1)
-            pyautogui.moveTo(pos_block_2.x(), int(0.9 * pos_block_2.y()))
-            pyautogui.mouseUp(button="left")
+    #         time.sleep(0.1)
+    #         pyautogui.moveTo(pos_block_2.x(), pos_block_2.y())
+    #         pyautogui.mouseDown(button="left")
+    #         time.sleep(0.1)
+    #         pyautogui.moveTo(pos_block_2.x(), int(0.9 * pos_block_2.y()))
+    #         pyautogui.mouseUp(button="left")
 
-            time.sleep(0.1)
-            pyautogui.moveTo(center_block_1.x(), center_block_1.y())
-            pyautogui.click()
-            time.sleep(0.1)
-            with pyautogui.hold("ctrl"):
-                pyautogui.press("z")
+    #         time.sleep(0.1)
+    #         pyautogui.moveTo(center_block_1.x(), center_block_1.y())
+    #         pyautogui.click()
+    #         time.sleep(0.1)
+    #         with pyautogui.hold("ctrl"):
+    #             pyautogui.press("z")
 
-            time.sleep(0.1)
-            # Undo in the 1st edited block should only undo in that block
-            msgQueue.check_equal(
-                self.code_block_1.source_editor.text().replace("\r", ""),
-                "ab\n",
-                "Undone selected editing",
-            )
+    #         time.sleep(0.1)
+    #         # Undo in the 1st edited block should only undo in that block
+    #         msgQueue.check_equal(
+    #             self.code_block_1.source_editor.text().replace("\r", ""),
+    #             "ab\n",
+    #             "Undone selected editing",
+    #         )
 
-            pyautogui.moveTo(pos_block_2.x(), int(0.75 * pos_block_2.y()))
-            pyautogui.click()
-            time.sleep(0.1)
-            with pyautogui.hold("ctrl"):
-                pyautogui.press("z", presses=2, interval=0.1)
+    #         pyautogui.moveTo(pos_block_2.x(), int(0.75 * pos_block_2.y()))
+    #         pyautogui.click()
+    #         time.sleep(0.1)
+    #         with pyautogui.hold("ctrl"):
+    #             pyautogui.press("z", presses=2, interval=0.1)
 
-            time.sleep(0.1)
-            # Need to relink after re-serialization
-            code_block_1: CodeBlock = self.widget.scene.getItemById(code_block_1_id)
-            code_block_2: CodeBlock = self.widget.scene.getItemById(code_block_2_id)
+    #         time.sleep(0.1)
+    #         # Need to relink after re-serialization
+    #         code_block_1: CodeBlock = self.widget.scene.getItemById(code_block_1_id)
+    #         code_block_2: CodeBlock = self.widget.scene.getItemById(code_block_2_id)
 
-            msgQueue.check_equal(
-                code_block_1.source_editor.text().replace("\r", ""),
-                "ab\na",
-                "Undone previous editing",
-            )
-            msgQueue.check_equal(
-                (code_block_2.pos().x(), code_block_2.pos().y()),
-                initial_pos,
-                "Undone graph modification",
-            )
-            msgQueue.check_equal(
-                code_block_2.source_editor.text().replace("\r", ""),
-                "cd\nd",
-                "Not undone 2 times previous editing",
-            )
+    #         msgQueue.check_equal(
+    #             code_block_1.source_editor.text().replace("\r", ""),
+    #             "ab\na",
+    #             "Undone previous editing",
+    #         )
+    #         msgQueue.check_equal(
+    #             (code_block_2.pos().x(), code_block_2.pos().y()),
+    #             initial_pos,
+    #             "Undone graph modification",
+    #         )
+    #         msgQueue.check_equal(
+    #             code_block_2.source_editor.text().replace("\r", ""),
+    #             "cd\nd",
+    #             "Not undone 2 times previous editing",
+    #         )
 
-            msgQueue.stop()
+    #         msgQueue.stop()
 
-        apply_function_inapp(self.window, testing_history)
+    #     apply_function_inapp(self.window, testing_history)
