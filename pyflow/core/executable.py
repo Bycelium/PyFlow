f145bd0aaee20eefe1714cac7b81c273bcd74545
Mathïs Fédérico <mathfederico@gmail.com>
2022-02-19 13:24:55 +0100

    :memo: Add executable docstrings

--- pyflow/core/executable.py
+++ pyflow/core/executable.py
@@ -1,7 +1,14 @@
+# Pyflow an open-source tool for modular visual programing in python
+# Copyright (C) 2021-2022 Bycelium <https://www.gnu.org/licenses/>
+
+""" Module for the Executable abstract class."""
+
 from enum import Enum
 
 
 class ExecutableState(Enum):
+    """Enumeration of possible states of an Executable."""
+
     IDLE = 0
     RUNNING = 1
     PENDING = 2
@@ -10,7 +17,10 @@ class ExecutableState(Enum):
 
 
 class Executable:
+    """Executable object in pyflow."""
+
     def __init__(self) -> None:
+        """Executable object in pyflow."""
         self._run_state = ExecutableState.IDLE
 
     @property
